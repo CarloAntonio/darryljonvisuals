eebd6287dd3c7d70075a0a31026609ab7b9f766d
Carlo Bilbao <cbilbao88@gmail.com>
2018-06-11 19:12:35 -0700

    chore: add new porfolio items, rearrange folder structure in aws

--- src/containers/home.js
+++ src/containers/home.js
@@ -2,33 +2,42 @@ import React from "react";
 
 const home = () => {
 
-const estate1 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/estatesSmall/estate1.jpg";
-const estate2 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/estatesSmall/estate2.jpg";
-const estate3 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/estatesSmall/estate3.jpg";
-const estate4 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/estatesSmall/estate4.jpg";
-const estate5 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/estatesSmall/estate5.jpg";
-const estate6 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/estatesSmall/estate6.jpg";
-const estate7 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/estatesSmall/estate7.jpg";
-const etonway = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/estatesSmall/etonway.jpg";
+//estates
+const estate2 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/estatesMed/estate2.jpg";
+const estate3 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/estatesMed/estate3.jpg";
+const estate4 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/estatesMed/estate4.jpg";
+const estate5 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/estatesMed/estate5.jpg";
+const estate6 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/estatesMed/estate6.jpg";
+const estate7 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/estatesMed/estate7.jpg";
 
-const life1 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/lifestyleSmall/life1.jpg";
-const life3 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/lifestyleSmall/life3.jpg";
-const life4 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/lifestyleSmall/life4.jpg";
-const life5 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/lifestyleSmall/life5.jpg";
-const life7 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/lifestyleSmall/life7.jpg";
-const life9 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/lifestyleSmall/life9.jpg";
-const marvkirs = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/lifestyleSmall/marvkirs.jpg";
-const johndenise = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/lifestyleSmall/johndenise.jpg";
+//lifestyle
+const carlo = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/lifestyleMed/life1.jpg";
+const life3 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/lifestyleMed/life3.jpg";
+const life4 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/lifestyleMed/life4.jpg";
+const life5 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/lifestyleMed/life5.jpg";
+const life7 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/lifestyleMed/life7.jpg";
+const life9 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/lifestyleMed/life9.jpg";
+const marvkirs = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/lifestyleMed/marvkirs.jpg";
+const johndenise = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/lifestyleMed/johndenise.jpg";
 
-const social1 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/socialSmall/social1.jpg";
-const social2 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/socialSmall/social2.jpg";
-const social3 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/socialSmall/social3.jpg";
-const social4 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/socialSmall/social4.jpg";
-const social5 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/socialSmall/social5.jpg";
-const social6 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/socialSmall/social6.jpg";
-const shreddy = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/socialSmall/shreddy.jpg";
-const mustang = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/socialSmall/mustang.jpg";
-  
+//social
+const social1 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/socialMed/social1.jpg";
+const social2 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/socialMed/social2.jpg";
+const social3 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/socialMed/social3.jpg";
+const social4 = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/socialMed/social4.jpg";
+
+//video covers
+const hisurfcamper = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/cover/hisurfcamperjpg";
+const perfectlocks = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/cover/perfectlocks.jpg";
+const donner = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/cover/donner.jpg";
+const etonway = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/cover/etonway.jpg";
+const gotfrenchie = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/cover/gotfrenchie.jpg";
+const johio = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/cover/johio.jpg";
+const mustang = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/cover/mustang.jpg";
+const shreddy = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/cover/shreddy.jpg";
+const wildman = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/cover/wildman.jpg";
+const chenin = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/darryljonvisuals/images/cover/chenin.jpg";
+
   return (
     <div>
 
@@ -79,7 +88,7 @@ const mustang = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/dar
                   <article className="work-item smedia video">
                     <div className="work-wrapper">
                       <div className="work-thumbnail">
-                        <img src={ social6 } alt=""/>
+                        <img src={ gotfrenchie } alt=""/>
                       </div>
                       <div className="work-caption">
                         <h3 className="work-title font-alt">@gotfrenchie</h3>
@@ -124,7 +133,7 @@ const mustang = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/dar
                   <article className="work-item smedia video">
                     <div className="work-wrapper">
                       <div className="work-thumbnail">
-                        <img src={ social5 } alt=""/>
+                        <img src={ johio } alt=""/>
                       </div>
                       <div className="work-caption">
                         <h3 className="work-title font-alt">@JOhio</h3>
@@ -139,13 +148,13 @@ const mustang = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/dar
                   <article className="work-item lifestyle">
                     <div className="work-wrapper">
                       <div className="work-thumbnail">
-                        <img src={ life1 } alt=""/>
+                        <img src={ carlo } alt=""/>
                       </div>
                       <div className="work-caption">
                         <h3 className="work-title font-alt">Carlo Bilbao</h3>
                         <span className="work-category font-serif">Website Assets</span>
                       </div>
-                      <a href={ life1 } className="work-link image-popup-vertical-fit"></a>
+                      <a href={ carlo } className="work-link image-popup-vertical-fit"></a>
                     </div>
                   </article>
                   {/* /PORTFOLIO ITEM */}
@@ -266,21 +275,6 @@ const mustang = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/dar
                         <span className="work-category font-serif">Fashion</span>
                       </div>
                       <a href={ life5 }  className="work-link image-popup-vertical-fit"></a>
-                    </div>
-                  </article>
-                  {/* /PORTFOLIO ITEM */}
-
-                  {/* PORTFOLIO ITEM */}
-                  <article className="work-item estate video">
-                    <div className="work-wrapper">
-                      <div className="work-thumbnail">
-                        <img src={ estate1 } alt=""/>
-                      </div>
-                      <div className="work-caption">
-                        <h3 className="work-title font-alt">Lisa Ellington</h3>
-                        <span className="work-category font-serif">Real Estate, Video</span>
-                      </div>
-                      <a href="https://www.youtube.com/watch?v=EchukbEvdm8"  className="work-link popup-youtube"></a>
                     </div>
                   </article>
                   {/* /PORTFOLIO ITEM */}
@@ -431,6 +425,81 @@ const mustang = "https://s3-us-west-1.amazonaws.com/carlo-central-cdn-bucket/dar
                         <span className="work-category font-serif">Real Estate</span>
                       </div>
                       <a href={ estate2 } className="work-link image-popup-vertical-fit"></a>
+                    </div>
+                  </article>
+                  {/* /PORTFOLIO ITEM */}
+
+                  {/* PORTFOLIO ITEM */}
+                  <article className="work-item smedia video">
+                    <div className="work-wrapper">
+                      <div className="work-thumbnail">
+                        <img src={ hisurfcamper } alt=""/>
+                      </div>
+                      <div className="work-caption">
+                        <h3 className="work-title font-alt">@HISurfCamper</h3>
+                        <span className="work-category font-serif">Promotional, Video</span>
+                      </div>
+                      <a href="https://www.youtube.com/watch?v=GwZW6v1lDK4" className="popup-youtube work-link"></a>
+                    </div>
+                  </article>
+                  {/* /PORTFOLIO ITEM */}
+
+                  {/* PORTFOLIO ITEM */}
+                  <article className="work-item smedia video">
+                    <div className="work-wrapper">
+                      <div className="work-thumbnail">
+                        <img src={ perfectlocks } alt=""/>
+                      </div>
+                      <div className="work-caption">
+                        <h3 className="work-title font-alt">@perfectlocks</h3>
+                        <span className="work-category font-serif">Promotional, Video</span>
+                      </div>
+                      <a href="https://www.youtube.com/watch?v=VkYdWBiYqn0" className="popup-youtube work-link"></a>
+                    </div>
+                  </article>
+                  {/* /PORTFOLIO ITEM */}
+
+                  {/* PORTFOLIO ITEM */}
+                  <article className="work-item smedia video">
+                    <div className="work-wrapper">
+                      <div className="work-thumbnail">
+                        <img src={ donner } alt=""/>
+                      </div>
+                      <div className="work-caption">
+                        <h3 className="work-title font-alt">Donner Summer</h3>
+                        <span className="work-category font-serif">Promotional, Video</span>
+                      </div>
+                      <a href="https://www.youtube.com/watch?v=mbRRx2bk0wk" className="popup-youtube work-link"></a>
+                    </div>
+                  </article>
+                  {/* /PORTFOLIO ITEM */}
+
+                  {/* PORTFOLIO ITEM */}
+                  <article className="work-item smedia video">
+                    <div className="work-wrapper">
+                      <div className="work-thumbnail">
+                        <img src={ wildman } alt=""/>
+                      </div>
+                      <div className="work-caption">
+                        <h3 className="work-title font-alt">@5dimensionalMan</h3>
+                        <span className="work-category font-serif">Promotional, Video</span>
+                      </div>
+                      <a href="https://www.youtube.com/watch?v=YrWcVyiTvxg" className="popup-youtube work-link"></a>
+                    </div>
+                  </article>
+                  {/* /PORTFOLIO ITEM */}
+
+                  {/* PORTFOLIO ITEM */}
+                  <article className="work-item estate video">
+                    <div className="work-wrapper">
+                      <div className="work-thumbnail">
+                        <img src={ chenin } alt=""/>
+                      </div>
+                      <div className="work-caption">
+                        <h3 className="work-title font-alt">Chenin</h3>
+                        <span className="work-category font-serif">Promotional, Video</span>
+                      </div>
+                      <a href="https://www.youtube.com/watch?v=hlySv4Kfh7o" className="popup-youtube work-link"></a>
                     </div>
                   </article>
                   {/* /PORTFOLIO ITEM */}
